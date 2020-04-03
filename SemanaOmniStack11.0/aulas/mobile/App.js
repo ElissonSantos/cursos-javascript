addd69c54748c4a8f06c9f19fe3e005bf701d5bf
andregodasi <andre.goncalves@wises.com.br>
2020-04-02 23:50:42 -0300

    Mobile Semana OmniStack

--- SemanaOmniStack11.0/aulas/mobile/App.js
+++ SemanaOmniStack11.0/aulas/mobile/App.js
@@ -1,21 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "intl";
+import "intl/locale-data/jsonp/pt-BR";
+
+import React from "react";
+
+import Routes from "./src/routes";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello Word!</Text>
-      <Text>Sem bug?!</Text>
-      <Text>AGORA VAAI!</Text>
-    </View>
-  );
+  return <Routes />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
