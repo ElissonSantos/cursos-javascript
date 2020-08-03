53b2b373a96787cf285ae4098062c433e7b2bd51
elissonSantos <elissonmaycon@gmail.com>
2020-08-03 15:55:08 -0300

    Aula2-Imersao-React

--- Imersao-React-Alura/elisflix/src/components/Menu/index.js
+++ Imersao-React-Alura/elisflix/src/components/Menu/index.js
@@ -1,19 +1,19 @@
 import React from "react";
-
-import "./Menu.css";
+import { Link } from "react-router-dom";
 
 import Logo from "../../assets/img/Logo.png";
+import "./Menu.css";
 import Button from "../Button";
 
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
-        <img className="Logo" src={Logo} alt="Elisflix" />
-      </a>
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
 
-      <Button as="a" className="ButtonLink" href="/">
-        Novo Video
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
       </Button>
     </nav>
   );
